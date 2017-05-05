b2e31d87357dbf063e650913336ac5fc736c9d85
Leonardo Cecchi <leonardo.cecchi@2ndquadrant.it>
2017-05-05 11:02:29 +0200

    laptop windows definition

---
+++ laptop.bat
@@ -0,0 +1,5 @@
+REM Install all the required software
+choco install -y git googlechrome firefox python2 python pycharm-community git putty notepadplusplus 
+
+REM Virtualization software
+choco install -y virtualbox vagrant
