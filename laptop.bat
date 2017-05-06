e92abfbc6b639f764a0d657b0a7ce9b8c86ad168
Leonardo Cecchi <leonardoce@interfree.it>
2017-05-06 18:56:38 +0200

    Add packages

--- laptop.bat
+++ laptop.bat
@@ -1,5 +1,16 @@
+REM This patch file will install in a Windows 10 based computer
+REM all the software required to work.
+REM With this environment, you can use a Linux VM easily and also
+REM develop with Python, PHP, C++, C#
+
 REM Install all the required software
-choco install -y git googlechrome firefox python2 python pycharm-community git putty notepadplusplus 
+choco install -y git googlechrome firefox python2 python pycharm-community git putty notepadplusplus winscp filezilla vim emacs sublimetext3 ccleaner
+
+REM Visual Studio
+choco install -y visualstudio2017Community visualstudio2017-workload-vctools visualstudio2017-workload-manageddesktop visualstudio2017-workload-netweb
 
 REM Virtualization software
-choco install -y virtualbox vagrant
+choco install -y virtualbox
+
+REM PHP related stuff
+choco install -y php
