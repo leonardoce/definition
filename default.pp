a8249ddbcab2089b744653e18ed1a85b30b5f5bf
Leonardo Cecchi <leonardoce@interfree.it>
2017-07-05 14:30:01 +0200

    Add pgenv

--- default.pp
+++ default.pp
@@ -69,19 +69,95 @@ class pyenv_development (
   }
 }
 
-# Tool composing a development environment
+# Pgenv installation
+class pgenv_development (
+  $username,
+  $home_directory,
+  $install_packages = true,
+  $postgres_version = "master") {
+
+  exec { "pgenv_install":
+    command => "/usr/bin/git clone https://github.com/leonardoce/pgenv ${home_directory}/pgsql",
+    cwd => "${home_directory}",
+    user => "${username}",
+    creates => "${home_directory}/pgsql",
+    require => Package['git']
+  } -> exec { "postgresql_clone":
+    command => "/usr/bin/git clone git://git.postgresql.org/git/postgresql.git ${home_directory}/pgsql/master",
+    user => "${username}",
+    creates => "${home_directory}/pgsql/master",
+  } -> file_line { 'pgenv_initialize_sh':
+    path => "${home_directory}/.bashrc",
+    line => "source ${home_directory}/pgsql/pgenv.sh",
+  }
+
+  if ($install_packages) {
+    package { "tcl-dev": ensure => "present" }
+    package { "libssl-dev": ensure => "present" }
+    package { "build-essential": ensure => "present" }
+    package { "bison": source => "present" }
+    package { "flex": source => "present" }
+    package { "libreadline-dev": source => "present" }
+    package { "libxml2-dev": source => "present" }
+  }
+
+  exec { "pgenv_configure_master":
+    command => "${home_directory}/pgsql/configure-all.sh master",
+    cwd => "${home_directory}/pgsql/",
+    user => "${username}",
+    creates => "${home_directory}/pgsql/master/config.log",
+    require => [Exec["pgenv_install"], Package['tcl-dev'], 
+	Package['libssl-dev'], Package['build-essential'], 
+	Package['bison'], Package['flex'], 
+	Package['libreadline-dev'], Package['libxml2-dev']],
+    environment => [ "HOME=${home_directory}" ],
+  } -> exec { "pgenv_compile_master":
+    command => "${home_directory}/pgsql/install-all.sh master",
+    cwd => "${home_directory}/pgsql/",
+    user => "${username}",
+    creates => "${home_directory}/.pgenv/versions/master",
+    environment => [ "HOME=${home_directory}" ],
+  }
+}
+
+# Tool composing my development environment
 class leonardo_development ( $username, $home_directory ) {
+  package { "bison": source => "present" }
+  package { "build-essential": ensure => "present" }
+  package { 'curl': ensure => 'present' }
+  package { "emacs-nox": ensure => "present" }
+  package { "flex": source => "present" }
+  package { "git": ensure => "present" }
+  package { 'libbz2-dev': ensure => 'present' }
+  package { 'libncurses5-dev': ensure => 'present' }
+  package { "libreadline-dev": source => "present" }
+  package { 'libsqlite3-dev': ensure => 'present' }
+  package { "libssl-dev": ensure => "present" }
+  package { "libxml2-dev": source => "present" }
+  package { 'llvm': ensure => 'present' }
+  package { 'make': ensure => 'present' }
+  package { "tcl-dev": ensure => "present" }
+  package { 'tk-dev': ensure => 'present' }
+  package { "vim": ensure => "present" }
+  package { 'wget': ensure => 'present' }
+  package { 'xz-utils': ensure => 'present' }
+  package { 'zlib1g-dev': ensure => 'present' }
+
+  # Development environment
   class { "nvm_development":
     username => $username,
     home_directory => $home_directory
   }
   class { "pyenv_development":
     username => $username,
-    home_directory => $home_directory
+    home_directory => $home_directory,
+    install_packages => false,
   }
-  package { "git": ensure => "present" }
-  package { "vim": ensure => "present" }
-  package { "emacs-nox": ensure => "present" }
+  class { "pgenv_development":
+    username => $username,
+    home_directory => $home_directory,
+    install_packages => false,
+  }
 
   # Installing Spacemacs
   exec { "install_spacemacs":
@@ -114,3 +190,7 @@ class { "leonardo_development" :
 }
 
 class { "leonardo_gui" : }
+
+# Useful packages
+package { "openssh-server": ensure => "present" }
+package { "tmux": ensure => "present" }
